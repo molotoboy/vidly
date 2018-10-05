4c9f61f255f3d4451a8cd8d4a463f92528391402
molotoboy <molotoboy@gmail.com>
2018-10-05 21:52:56 +0600

    lists

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,6 +11,26 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1,Name="Shrek!"},
+                new Movie {Id=2,Name="Wall-e"}
+            };
+        }
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
@@ -19,16 +39,18 @@ namespace Vidly.Controllers
                 new Customer{Name="Customer 1"},
                 new Customer{Name="Customer 1"}
             };
-            var viewModel = new RandomMovieViewModel {
-                Movie=movie,
-                Customers=customers
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
             };
             return View(viewModel);
         }
         [Route("movies/released/{year:regex(\\d{4}):range(1900,2018)}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
-            return Content(year+"/"+month);
+            return Content(year + "/" + month);
         }
+
     }
 }
