ca204aeb5e3c029cf40d5f46ba1862e72df5834e
molotoboy <molotoboy@gmail.com>
2018-10-05 07:58:45 +0600

    update1

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 1"}
+            };
+            var viewModel = new RandomMovieViewModel {
+                Movie=movie,
+                Customers=customers
+            };
+            return View(viewModel);
+        }
+        [Route("movies/released/{year:regex(\\d{4}):range(1900,2018)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+month);
+        }
+    }
+}
